4173d30ca5821a63139587c7a616c4e178808f2d
kingston-twelve <kingston.yip@twelvelabs.io>
2025-03-11 18:29:54 -0700

    example outputs

---
+++ example_output/project_config.py
@@ -0,0 +1,7 @@
+# Project config saved on 2025-03-11T18:22:03.566357
+
+PROJECT = '250219_nif_ndby_LKW_Crash'
+LOCAL_VIDEO_PATH = '~/Downloads/12labs/250219_nif_ndby_LKW_Crash/250219_News5_LKW_Crash_A3_1.mp4'
+RAW_NEWS_FILE_PATH = '~/Downloads/12labs/250219_nif_ndby_LKW_Crash/translated_raw.txt'
+TWELVELABS_INDEX_ID = '67cf4c17c14a54d6e58d1388'
+TWELVELABS_VIDEO_ID = '67cf69a8f45d9b64a583534c'
